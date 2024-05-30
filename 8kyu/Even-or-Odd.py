0c7988e6a4dcc3e5f0753c61d28800e218ead51e
pini91 <brenda.loncaric@gmail.com>
2024-05-30 10:27:49 -0500

    Adding new coding challenge

---
+++ 8kyu/Even-or-Odd.py
@@ -0,0 +1,10 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number):
+    if number%2==0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+even_or_odd(2)
