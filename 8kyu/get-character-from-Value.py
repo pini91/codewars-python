243c96f32c3391ffa97c3d6b8f5a264993bd420d
pini91 <brenda.loncaric@gmail.com>
2025-06-12 13:43:06 -0400

    Adding new codewars challenge.

---
+++ 8kyu/get-character-from-Value.py
@@ -0,0 +1,12 @@
+# Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+# Example:
+
+# 65 --> 'A'
+# 97 --> 'a'
+# 48 --> '0
+
+#ord('h') if we wanted the number of the value
+
+def get_char(c):
+    return chr(c)
