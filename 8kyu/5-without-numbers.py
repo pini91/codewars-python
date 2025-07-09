bed19ff5c412b42e0e0440a855e9ac7a75e5548f
pini91 <brenda.loncaric@gmail.com>
2025-07-09 14:10:02 -0400

    Adding new codewars challenge.

---
+++ 8kyu/5-without-numbers.py
@@ -0,0 +1,15 @@
+# Write a function that always returns 5
+
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+# Good luck :)
+
+def unusual_five():
+    five = 'brenda'
+    letter ='a'
+    return five.index(letter) 
+
+# or
+# return len('five!')
+
+print(unusual_five())
