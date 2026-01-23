4aad3546167426e5305c5f84c60af80e764719cb
pini91 <brenda.loncaric@gmail.com>
2026-01-23 14:32:46 -0500

    Adding new codewars challenge.

---
+++ 8kyu/Simple-validation-of-a-username-with-regex.py
@@ -0,0 +1,14 @@
+# Write a simple regex to validate a username. Allowed characters are:
+
+# lowercase letters,
+# numbers,
+# underscore
+# Length should be between 4 and 16 characters (both included).
+import re
+
+def validate_usr(username):
+    # print(len(username))
+    return bool(re.search('^[a-z\d_]{4,16}$', username))
+
+
+print(validate_usr('asd1ds1a_'))
